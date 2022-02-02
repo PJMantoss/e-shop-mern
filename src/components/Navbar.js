f81f66b2b276e431d86011f4b690c66a53a9a3f0
PJMantoss <joelptoss@gmail.com>
2022-02-02 12:32:54 +0100

    update Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    height: 60px;
+    background-color: black;
+`;
+
+const Wrapper = styled.div`
+    padding: 10px 20px;
+`;
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return <Container>Navbar</Container>;
 };
 
 export default Navbar;
