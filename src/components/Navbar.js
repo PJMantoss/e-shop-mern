a660a3a23d40401116dd696c610fd1bb9c93c026
PJMantoss <joelptoss@gmail.com>
2022-02-03 16:33:56 +0100

    update Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -27,6 +27,8 @@ const SearchContainer = styled.div`
     border: 1px solid lightgray;
     display: flex;
     align-items: center;
+    margin-left: 25px;
+    padding: 5px;
 `;
 
 const Center = styled.div`flex:1;`;
