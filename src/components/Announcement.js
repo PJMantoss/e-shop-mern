efed31bd73527a8fc53d8af3f4b918b2a8b2413f
PJMantoss <joelptoss@gmail.com>
2022-02-03 17:49:15 +0100

    update Announcement component

--- src/components/Announcement.js
+++ src/components/Announcement.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Container = styled.div``;
+const Container = styled.div`
+    height: 30px;
+    background-color: teal;
+    color: white;
+`;
 
 export const Announcement = () => {
   return <div></div>;
