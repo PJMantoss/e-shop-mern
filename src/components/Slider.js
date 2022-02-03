535b7f90de48917303a0de86afbf836d52a2ad55
PJMantoss <joelptoss@gmail.com>
2022-02-03 17:59:06 +0100

    update Slider component

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -3,14 +3,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 30px;
-    background-color: teal;
-    color: white;
-    font-size: 14px;
-    font-weight: bold;
+    width: 100vw;
 `;
 
 const Slider = () => {
