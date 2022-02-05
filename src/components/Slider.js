6839bee54fc24e1d9c39d54ff5cf6ab9296e6a6d
PJMantoss <joelptoss@gmail.com>
2022-02-05 19:55:26 +0100

    update Slider component

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ArrowLeftOutlinedIcon from '@mui/icons-material/ArrowLeftOutlined';
 import ArrowRightOutlinedIcon from '@mui/icons-material/ArrowRightOutlined';
 
