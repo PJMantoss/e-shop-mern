0303cf85a7f9c59c8a2d93d223e7f6c5773d4c67
PJMantoss <joelptoss@gmail.com>
2022-02-05 20:04:26 +0100

    update data.js

--- src/data.js
+++ src/data.js
@@ -1,14 +1,14 @@
 export const sliderItems = [
     {
         id: 1,
-        image: "",
+        image: "https://images.pexels.com/photos/1926769/pexels-photo-1926769.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
         title: "SUMMER SALE",
         desc: "Don't Compromise on Style. Get 30% Off!",
         bg: "f5fafd"
     },
     {
         id: 2,
-        image: "",
+        image: "https://images.pexels.com/photos/1926769/pexels-photo-1926769.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
         title: "AUTUMN COLLECTION",
         desc: "Don't Compromise on Style. Get 30% Off!",
         bg: "fcf1ed"
