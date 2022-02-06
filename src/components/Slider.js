92be92a75619b575a2d6aa606d89b36792d53989
PJMantoss <joelptoss@gmail.com>
2022-02-06 23:36:26 +0100

    update Slider.js

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import ArrowLeftOutlinedIcon from '@mui/icons-material/ArrowLeftOutlined';
 import ArrowRightOutlinedIcon from '@mui/icons-material/ArrowRightOutlined';
+import { sliderItems } from '../data';
 
 import styled from 'styled-components';
 
