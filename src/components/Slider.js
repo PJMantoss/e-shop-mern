33d635dabd856b8591b9e587cf255254746b679a
PJMantoss <joelptoss@gmail.com>
2022-02-06 23:39:39 +0100

    update Slider.js

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -91,17 +91,6 @@ const Slider = () => {
               </Arrow>
               <Wrapper>
                   {sliderItems.map(item => ())}
-                  <Slide bg="f5fafd">
-                    <ImgContainer>
-                        <Image src="https://images.pexels.com/photos/1926769/pexels-photo-1926769.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
-                    </ImgContainer>
-
-                    <InfoContainer>
-                        <Title>Summer Sales</Title>
-                        <Desc>Don't Compromise on Style. Get 30% Off!</Desc>
-                        <Button>Shop Now</Button>
-                    </InfoContainer>
-                  </Slide>
               </Wrapper>
               <Arrow direction="right" onClick={() => handleClick("right")}>
                   <ArrowRightOutlinedIcon />
