8187df9965e86f687df53aa2a5eea29851e529ff
PJMantoss <joelptoss@gmail.com>
2022-02-09 18:22:46 +0100

    update CategoryItem.js

--- src/components/CategoryItem.js
+++ src/components/CategoryItem.js
@@ -2,6 +2,11 @@ import React from 'react';
 
 import styled from 'styled-components';
 
+const Container = styled.div`
+    display: flex;
+    padding: 20px;
+`;
+
 const CategoryItem = ({item}) => {
   return (
     <Container></Container>
