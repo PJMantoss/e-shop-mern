ca8fdb1541644919d714b1a265b81cbbb02ea3e7
PJMantoss <joelptoss@gmail.com>
2022-02-09 18:47:13 +0100

    update CategoryItem.js

--- src/components/CategoryItem.js
+++ src/components/CategoryItem.js
@@ -5,10 +5,14 @@ import styled from 'styled-components';
 const Container = styled.div`
     flex: 1;
     margin: 3px;
+    height: 70vh;
+    position: relative;
 `;
 
 const Image = styled.img`
     width: 100%;
+    height: 100%;
+    object-fit: cover;
 `;
 
 const Info = styled.div``;
