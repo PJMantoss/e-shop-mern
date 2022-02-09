db9eac3f7a84ae50a5549d72578794ba97d90510
PJMantoss <joelptoss@gmail.com>
2022-02-09 18:36:16 +0100

    update CategoryItem.js

--- src/components/CategoryItem.js
+++ src/components/CategoryItem.js
@@ -8,6 +8,7 @@ const Container = styled.div`
 const Image = styled.img``;
 const Info = styled.div``;
 const Title = styled.p``;
+const Button = styled.button``;
 
 const CategoryItem = ({item}) => {
   return (
