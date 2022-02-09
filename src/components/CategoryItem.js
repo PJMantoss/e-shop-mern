69bc33d1ae71540e699f93aeab70e3a608283888
PJMantoss <joelptoss@gmail.com>
2022-02-09 18:40:55 +0100

    update CategoryItem.js

--- src/components/CategoryItem.js
+++ src/components/CategoryItem.js
@@ -13,7 +13,7 @@ const Button = styled.button``;
 const CategoryItem = ({item}) => {
   return (
     <Container>
-        <Image src={} />
+        <Image src={item.img} />
         <Info>
             <Title> {item.title} </Title>
             <Button> SHOP NOW </Button>
