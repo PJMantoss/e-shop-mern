07a725fc470bcc1fe0f9e70fa9c671277a522551
PJMantoss <joelptoss@gmail.com>
2022-02-09 18:20:44 +0100

    update Categories.js

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -4,7 +4,8 @@ import CategoryItem from './CategoryItem';
 
 import styled from 'styled-components';
 
-const Container = styled.div``;
+const Container = styled.div`
+`;
 
 const Categories = () => {
   return (
