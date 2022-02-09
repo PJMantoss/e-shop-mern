506a5feb36d431ccc642be30da8307b7f3f76e56
PJMantoss <joelptoss@gmail.com>
2022-02-09 18:06:32 +0100

    update data.js

--- src/data.js
+++ src/data.js
@@ -33,4 +33,9 @@ export const categories = [
         img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
         title: "LOUNGEWEAR LOVE",
     },
+    {
+        id: 3,
+        img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        title: "LIGHT JACKETS",
+    },
 ];
