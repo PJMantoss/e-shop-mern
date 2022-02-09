c583adfad84de9ac99af5fde2e6df1018fb9779a
PJMantoss <joelptoss@gmail.com>
2022-02-09 18:53:30 +0100

    update CategoryItem.js

--- src/components/CategoryItem.js
+++ src/components/CategoryItem.js
@@ -22,7 +22,9 @@ const Info = styled.div`
     width: 100%;
     height: 100%;
     display: flex;
+    flex-direction: column;
     align-items: center;
+    justify-content: center;
 `;
 
 const Title = styled.h1``;
