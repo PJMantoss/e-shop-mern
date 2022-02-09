39ba61cb43081e8d2a6e344b7b4e5fdba93657c4
PJMantoss <joelptoss@gmail.com>
2022-02-09 17:36:25 +0100

    update Slider.js

--- src/data.js
+++ src/data.js
@@ -8,7 +8,7 @@ export const sliderItems = [
     },
     {
         id: 2,
-        image: "https://images.pexels.com/photos/1926769/pexels-photo-1926769.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        image: "https://i.ibb.co/DG69bQ4/2.png",
         title: "AUTUMN COLLECTION",
         desc: "Don't Compromise on Style. Get 30% Off!",
         bg: "fcf1ed"
