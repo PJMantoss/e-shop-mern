ab1209db2819486dce23b23fa572bc57a3fa5b66
PJMantoss <joelptoss@gmail.com>
2022-02-10 09:51:40 +0100

    update data

--- src/data.js
+++ src/data.js
@@ -57,4 +57,12 @@ export const popularProducts = [
         id:4,
         img:"https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
     },
+    {
+        id:5,
+        img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+        id:6,
+        img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+    },
 ];
