cd844c5d50c2c90c6230804e30c386d4031e879d
PJMantoss <joelptoss@gmail.com>
2022-02-10 10:31:04 +0100

    update Categories

--- src/components/Product.js
+++ src/components/Product.js
@@ -7,9 +7,10 @@ import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlin
 import styled from 'styled-components';
 
 const Container = styled.div`
-    display: flex;
-    margin: 5;
+    flex: 1;
+    margin: 5px;
     min-width: 280px;
+    height: 350px;
 `;
 
 const Circle = styled.div``;
