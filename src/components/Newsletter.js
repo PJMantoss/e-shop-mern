defd99e397d733fe549df4208b50d832082ff24a
PJMantoss <joelptoss@gmail.com>
2022-02-11 12:01:36 +0100

    update Newsletter

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -7,6 +7,9 @@ const Newsletter = () => {
     <Container>
         <Title />
         <Description />
+        <InputContainer>
+            <Input />
+        </InputContainer>
     </Container>
   )
 }
