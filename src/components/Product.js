a7562ac7264bc518a7898888a693601df0e373e2
PJMantoss <joelptoss@gmail.com>
2022-02-11 11:41:21 +0100

    update Product

--- src/components/Product.js
+++ src/components/Product.js
@@ -35,8 +35,8 @@ const Info = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
-    width: 100px;
-    height: 100px;
+    width: 100%;
+    height: 100%;
     position: absolute;
     top: 0;
     left: 0;
