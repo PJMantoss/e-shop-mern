dea785baf7ed651f8f9514583b930ba012c5faf1
PJMantoss <joelptoss@gmail.com>
2022-02-11 12:14:48 +0100

    add Newsletter to Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import Announcement from '../components/Announcement';
 import Slider from '../components/Slider';
 import Categories from '../components/Categories';
 import Products from '../components/Products';
+import Newsletter from '../components/Newsletter';
 
 const Home = () => {
   return (
