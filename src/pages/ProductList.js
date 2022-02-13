91d0a95213ee1ff8b40b5dd8f645bd66f56f179c
PJMantoss <joelptoss@gmail.com>
2022-02-13 12:45:05 +0100

    update ProductList

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 import styled from 'styled-components';
 
+const Container = styled.div``;
+
 const ProductList = () => {
   return (
-    <div>ProductList</div>
+    <Container>ProductList</Container>
   )
 }
 
