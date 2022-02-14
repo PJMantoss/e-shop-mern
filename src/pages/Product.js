e0d7e147f764c55a30b193b7f8b5dd9fe993f94c
PJMantoss <joelptoss@gmail.com>
2022-02-14 09:08:33 +0100

    update Product page

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import Announcement from '../components/Announcement';
+import Navbar from '../components/Navbar';
+import Newsletter from '../components/Newsletter';
+import Footer from '../components/Footer';
 
 import styled from 'styled-components';
 
