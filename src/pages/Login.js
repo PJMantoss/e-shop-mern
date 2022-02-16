fcd058e7a9bf22e7bfaa91b284a3a94145bd5a3c
PJMantoss <joelptoss@gmail.com>
2022-02-16 14:18:50 +0100

    update Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -54,7 +54,22 @@ const Button = styled.button`
 const Login = () => {
   return (
     <Container>
-
+      <Wrapper>
+        <Title>SIGN IN</Title>
+        <Form>
+          <Input placeholder="first name" />
+          <Input placeholder="last name" />
+          <Input placeholder="email" />
+          <Input placeholder="username" />
+          <Input placeholder="password" />
+          <Input placeholder="confirm password" />
+          <Agreement>
+            By creating an account, I agree to the processing of my personal data
+            in accordance with the <b>Privacy Policy</b>.
+          </Agreement>
+          <Button>REGISTER</Button>
+        </Form>
+      </Wrapper>
     </Container>
   )
 }
