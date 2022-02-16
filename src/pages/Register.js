08e07cef7685ed27aa99c1ceee08efd1151107bf
PJMantoss <joelptoss@gmail.com>
2022-02-16 13:55:44 +0100

    update Register

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 import styled from 'styled-components';
 
-const Container = styled.div``;
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
 
 const Wrapper = styled.div``;
 
