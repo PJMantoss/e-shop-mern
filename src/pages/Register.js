d88b059ffda1d8024aef9427d4838448f9c36f20
PJMantoss <joelptoss@gmail.com>
2022-02-16 14:05:07 +0100

    update Register

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -3,6 +3,9 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
   width: 100vw;
   height: 100vh;
   background: linear-gradient(
