02ade0543ac01f020566af9841b3a043bcace1d1
PJMantoss <joelptoss@gmail.com>
2022-02-16 13:38:07 +0100

    update Register

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from 'react';
+
+import styled from 'styled-components';
+
+const Container = styled.div``;
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <Container>Register</Container>
   )
 }
 
