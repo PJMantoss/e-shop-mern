84d32db1a68fab569f43d43cc9773a38aaae85e2
PJMantoss <joelptoss@gmail.com>
2022-02-16 14:17:42 +0100

    update Register

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,12 +2,59 @@ import React from 'react';
 
 import styled from 'styled-components';
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+    rgba(255, 255, 255, 0.5),
+    rgba(255, 255, 255, 0.5)
+  ), url("https://images.pexels.com/photos/6984661/pexels-photo-6984661.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940") center;
+`;
+
+const Wrapper = styled.div`
+  width: 40%;
+  padding: 20px;
+  background: white;
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 300;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 20px 10px 0 0;
+  padding: 10px;
+`;
+
+const Agreement = styled.span`
+    margin: 20px 0;
+    font-size: 12px;
+`;
+
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  background: teal;
+  padding: 15px 20px;
+  color: white;
+  cursor: pointer;
+`;
 
 const Login = () => {
   return (
     <Container>
-      
+
     </Container>
   )
 }
