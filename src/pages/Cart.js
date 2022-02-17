3c37cc40a4aacbd949211a2c42e5dc754242ea10
PJMantoss <joelptoss@gmail.com>
2022-02-17 17:29:40 +0100

    update Cart

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import Announcement from '../components/Announcement';
+import Navbar from '../components/Navbar';
 
 const Container = styled.div
 
@@ -8,6 +9,7 @@ const Cart = () => {
   return (
     <Container>
         <Announcement />
+        <Navbar />
         Cart
     </Container>
   )
