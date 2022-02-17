ca59dcdd377d877cbb14f49700b790ac89b04dcc
PJMantoss <joelptoss@gmail.com>
2022-02-17 17:30:55 +0100

    update Cart

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import styled from 'styled-components';
 import Announcement from '../components/Announcement';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+
+import styled from 'styled-components';
 
 const Container = styled.div
 
