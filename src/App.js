9ca71a43b9bb115c5b80d23a1434bbcd630d871c
PJMantoss <joelptoss@gmail.com>
2022-02-17 17:31:44 +0100

    update App

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import ProductList from './pages/ProductList';
 import Product from './pages/Product';
 import Register from './pages/Register';
 import Login from './pages/Login';
+import Cart from './pages/Cart';
 
 const App = () => {
   return (
