f895d3ccf18c1ab301d69d472d9da59ed0888158
PJMantoss <joelptoss@gmail.com>
2022-02-18 20:00:40 +0100

    update Cart

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -81,8 +81,8 @@ const Cart = () => {
                         <ProductDetail>
                             <Image src="ttps://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1614188818-TD1MTHU_SHOE_ANGLE_GLOBAL_MENS_TREE_DASHERS_THUNDER_b01b1013-cd8d-48e7-bed9-52db26515dc4.png?crop=1xw:1.00xh;center,top&resize=480%3A%2A" />
                             <Details>
-                                <ProductName> <b></b> </ProductName>
-                                <ProductId> <b></b> </ProductId>
+                                <ProductName> <b>Product:</b> </ProductName>
+                                <ProductId> <b>ID:</b> </ProductId>
                                 <ProductColor />
                                 <ProductSize> <b></b> </ProductSize>
                             </Details>
