09dcd132cf16f79fc1035eb8a0da00734c57331d
PJMantoss <joelptoss@gmail.com>
2022-02-21 09:26:57 +0100

    update responsive

--- src/responsive.js
+++ src/responsive.js
@@ -1,3 +1,9 @@
 import { css } from 'styled-components';
 
-export const mobile = props => {};
+export const mobile = props => {
+    return css`
+        @media only screen and (max-width: 380px){
+            display: none;
+        };
+    `;
+};
