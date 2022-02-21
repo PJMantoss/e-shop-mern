cd59f7e6e843f2676e9da776da105b102d215d63
PJMantoss <joelptoss@gmail.com>
2022-02-21 09:21:28 +0100

    update Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,7 @@ import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 
 const Container = styled.div`
     height: 60px;
+    @media only screen (max-width: 380px){}
 `;
 
 const Wrapper = styled.div`
