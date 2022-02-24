0170862abc86c70bfb1a724eed1ef24d94c76419
PJMantoss <joelptoss@gmail.com>
2022-02-24 13:27:53 +0100

    update server

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://mantoss:superstars@cluster0.kxgzl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://mantoss:superstars@cluster0.kxgzl.mongodb.net/shop?retryWrites=true&w=majority");
 
 app.listen(5000, () => {
     console.log("Backend Server is running!");
