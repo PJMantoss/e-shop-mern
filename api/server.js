c4f19790cc797f2ebe1f11bc0168f437c7f48b80
PJMantoss <joelptoss@gmail.com>
2022-02-24 13:38:56 +0100

    update server

--- api/server.js
+++ api/server.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 mongoose.connect(
     "mongodb+srv://mantoss:superstars@cluster0.kxgzl.mongodb.net/shop?retryWrites=true&w=majority"
     )
-    .then(() => console.log("DBConnection Successfull!"))
+    .then(() => console.log("DB Connection Successfull!"))
     .catch(err => console.log(err));
 
 app.listen(5000, () => {
