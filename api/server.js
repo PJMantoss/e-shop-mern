0e5a3172737cd39791d28f68c63b616d441584b8
PJMantoss <joelptoss@gmail.com>
2022-02-24 13:33:50 +0100

    update server

--- api/server.js
+++ api/server.js
@@ -6,7 +6,7 @@ mongoose.connect(
     "mongodb+srv://mantoss:superstars@cluster0.kxgzl.mongodb.net/shop?retryWrites=true&w=majority"
     )
     .then(() => console.log("DBConnection Successfull!"))
-    .catch();
+    .catch(err => console.log(err));
 
 app.listen(5000, () => {
     console.log("Backend Server is running!");
