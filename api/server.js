eda66b392e1ba94765b9638dc3382b96ba0185ea
PJMantoss <joelptoss@gmail.com>
2022-02-24 13:42:43 +0100

    update server

--- api/server.js
+++ api/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 mongoose.connect(
     "mongodb+srv://mantoss:superstars@cluster0.kxgzl.mongodb.net/shop?retryWrites=true&w=majority"
