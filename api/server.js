758df453463ed5bb7227d381e573842827a45c43
PJMantoss <joelptoss@gmail.com>
2022-02-25 21:21:53 +0100

    update server

--- api/server.js
+++ api/server.js
@@ -11,9 +11,7 @@ mongoose
     .then(() => console.log("DB Connection Successfull!"))
     .catch(err => console.log(err));
 
-app.get("/api/test/", () => {
-    console.log("Test Successful!")
-});
+app.use()
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("Backend Server is running!");
