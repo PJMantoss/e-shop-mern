6b28d2a0913c3d40587145dff87defd2fb71235e
PJMantoss <joelptoss@gmail.com>
2022-02-26 15:57:13 +0100

    update User models

--- api/models/User.js
+++ api/models/User.js
@@ -2,6 +2,6 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     username: {type: String, required: true, unique: true},
-    email: {},
+    email: {type: String, required: true, unique: true},
     password: {},
 });
