6c9a29038fa714455d021a30689e0aa8161e2150
PJMantoss <joelptoss@gmail.com>
2022-02-26 20:23:01 +0100

    update Product model

--- api/models/Product.js
+++ api/models/Product.js
@@ -3,8 +3,11 @@ const { boolean } = require("webidl-conversions");
 
 const ProductSchema = new mongoose.Schema({
     title: {type: String, required: true, unique: true},
-    desc: {type: String, required: true, unique: true},
-    img: {ype: String, required: true},
+    desc: {type: String, required: true},
+    img: {type: String, required: true},
+    size: {type: String, required: true},
+    color: {type: String, required: true},
+    price: {type: String, required: true},
 },
 {timestamps: true}
 );
