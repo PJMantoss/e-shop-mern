12ab1153aa6d42372f94c0e9703507e2949853d9
PJMantoss <joelptoss@gmail.com>
2022-02-27 15:27:46 +0100

    update authentication route

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 
 const User = require("../models/User");
 
+const CryptoJS = require("crypto-js");
+
 //Register
 router.post("/register", async (req, res) => {
     const newUser = new User({
