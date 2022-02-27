c9d7289d5acec804e706901ac139d3b3a56b077a
PJMantoss <joelptoss@gmail.com>
2022-02-27 14:34:17 +0100

    update authentication route

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -6,6 +6,8 @@ const User = require("../models/User");
 router.post("/register", (req, res) => {
     const newUser = new User({
         username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
     });
 });
 
