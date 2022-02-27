630ce378e2c75968ea5b33827f40f0d7ffba8744
PJMantoss <joelptoss@gmail.com>
2022-02-27 16:10:48 +0100

    update authentication route

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -21,6 +21,12 @@ router.post("/register", async (req, res) => {
 });
 
 //Login
-router.post("/login", async (req, res) => {});
+router.post("/login", async (req, res) => {
+    try{
+        const user = await User.findOne({ username: req.body.username })
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
