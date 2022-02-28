2fb929f83da8398b8a8216f3698c33fa57d957ed
PJMantoss <joelptoss@gmail.com>
2022-02-28 10:37:35 +0100

    update authentication route

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -4,8 +4,6 @@ const User = require("../models/User");
 
 const CryptoJS = require("crypto-js");
 
-const jwt = require("jasonwebtoken");
-
 //Register
 router.post("/register", async (req, res) => {
     const newUser = new User({
@@ -24,13 +22,11 @@ router.post("/register", async (req, res) => {
 
 //Login
 router.post("/login", async (req, res) => {
-    let user;
+    
     try{
-        if(user){
-            user = await User.findOne({ userName: req.body.user_name });
-        } else {
-            res.status(401).json("Wrong Credentials!");
-        }
+        const user = await User.findOne({ username: req.body.username });
+
+        !user && res.status(401).json("Wrong Credentials!");
 
         const hashedPassword = CryptoJS.AES.decrypt(user.password, process.env.PASSWORD_SECRET);
 
