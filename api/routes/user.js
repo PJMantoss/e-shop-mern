5f0478122a395ee9074322b801e012df4c838744
PJMantoss <joelptoss@gmail.com>
2022-02-28 11:07:16 +0100

    update user

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,8 +1,8 @@
-const verifyToken = require("./verifyToken");
+const {verifyToken} = require("./verifyToken");
 
 const router = require("express").Router();
 
-router.put("/:id", verifyToken);
+router.put("/:id", verifyToken, (req, res) => {});
 
 module.exports = router;
 
