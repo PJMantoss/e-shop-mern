3779ff3577f1514b98ba756827c507baddd94812
PJMantoss <joelptoss@gmail.com>
2022-02-28 10:43:50 +0100

    update authentication route

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -41,7 +41,7 @@ router.post("/login", async (req, res) => {
             isAdmin: user.isAdmin,
         }, 
         process.env.JWT_SECRET,
-        {},
+        {expiresIn: "3d"},
         );
 
         const { password, ...others } = user._doc;
