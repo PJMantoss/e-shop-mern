e8685af8e4822f93d4cb0fcb8bf2857f4e0fed9f
PJMantoss <joelptoss@gmail.com>
2022-03-01 12:01:46 +0100

    update verifyToken

--- api/routes/user.js
+++ api/routes/user.js
@@ -3,7 +3,9 @@ const {verifyToken, verifyTokenAndAuthorization} = require("./verifyToken");
 const router = require("express").Router();
 
 router.put("/:id", verifyTokenAndAuthorization, (req, res) => {
-    if(req.body.password){}
+    if(req.body.password){
+        password: CryptoJS.AES.encrypt(req.body.password, process.env.PASSWORD_SECRET).toString(),
+    }
 });
 
 module.exports = router;
