05367ccba63351553c7c9f09435c01534cb5a32f
PJMantoss <joelptoss@gmail.com>
2022-03-01 11:56:31 +0100

    update verifyToken

--- api/routes/verifyToken.js
+++ api/routes/verifyToken.js
@@ -17,6 +17,8 @@ const verifyTokenAndAuthorization = (req, res, next) => {
     verifyToken(req, res, () =>{
         if(req.user.id === req.params.id || req.user.isAdmin){
             next();
+        }else{
+            res.status(403).json("You are not allowed to do that!");
         }
     })
 };
