9646c6022f08023b0977dd4f99f217e2de2b9268
PJMantoss <joelptoss@gmail.com>
2022-03-01 11:53:01 +0100

    update user

--- api/routes/user.js
+++ api/routes/user.js
@@ -3,7 +3,7 @@ const {verifyToken} = require("./verifyToken");
 const router = require("express").Router();
 
 router.put("/:id", verifyToken, (req, res) => {
-    if(req.user.id === req.params.id || req.user.isAdmin){}
+    
 });
 
 module.exports = router;
