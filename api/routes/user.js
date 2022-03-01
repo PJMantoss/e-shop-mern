1d71ea688398da0e239d225357bb8e1ec21d779a
PJMantoss <joelptoss@gmail.com>
2022-03-01 12:08:29 +0100

    update user

--- api/routes/user.js
+++ api/routes/user.js
@@ -12,8 +12,19 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
     }
 
     try{
-        const updatedUser = await User.findByIdAndUpdate(req.params.id, {})
-    }catch(err){}
+        const updatedUser = await User.findByIdAndUpdate(
+            req.params.id, 
+            {
+                $set: req.id
+            },
+            {
+                new: true
+            },
+            res.status(200).json(updatedUser)
+            )
+    }catch(err){
+        res.status(500).json(err)
+    }
 });
 
 module.exports = router;
