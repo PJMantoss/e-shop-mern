0c3d3592d1b8d270b3ac001da4bb913b8efabd5e
PJMantoss <joelptoss@gmail.com>
2022-03-02 13:53:46 +0100

    update user

--- api/routes/user.js
+++ api/routes/user.js
@@ -44,7 +44,7 @@ router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
 });
 
 //GET USER
-router.get("", verifyTokenAndAdmin, async (req, res) => {
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
     try{
         const user = await User.findById(req.params.id)
         const { password, ...others } = user._doc;
