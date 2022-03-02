5677168c12f7fa0030371e5d8c76c86e2c8c6929
PJMantoss <joelptoss@gmail.com>
2022-03-02 13:13:54 +0100

    update user

--- api/routes/user.js
+++ api/routes/user.js
@@ -37,7 +37,17 @@ router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
     }catch(err){
         res.status(500).json(err);
     }
-})
+});
+
+//GET
+router.get("/:id", verifyTokenAndAuthorization, async (req, res) => {
+    try{
+        await User.findByIdAndDelete(req.params.id)
+        res.status(200).json("User has been deleted...")
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
 
