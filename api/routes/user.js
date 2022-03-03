8b841c27eaf3fcc637e81ba8593c6ed58a341c84
PJMantoss <joelptoss@gmail.com>
2022-03-03 10:54:40 +0100

    update user

--- api/routes/user.js
+++ api/routes/user.js
@@ -77,6 +77,8 @@ router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
                 }
             }
         ]);
+
+        res.status(200).json(data);
     }catch(err){
         res.status(500).json(err);
     };
