72ff054226d14ed9dd4bdbfc2355fc53ae50184a
PJMantoss <joelptoss@gmail.com>
2022-03-04 15:21:05 +0100

    update product route

--- api/routes/product.js
+++ api/routes/product.js
@@ -11,7 +11,7 @@ const router = require("express").Router();
 //CREATE
 router.post("/", verifyTokenAndAdmin, async (req, res) => {
     const newProduct = new Product(req.body);
-    
+
     try{
         const savedProduct = await newProduct.save();
         res.status(200).json(savedProduct);
@@ -21,30 +21,24 @@ router.post("/", verifyTokenAndAdmin, async (req, res) => {
 })
 
 //UPDATE
-// router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
-//     if(req.body.password){
-//         req.body.password= CryptoJS.AES.encrypt(
-//             req.body.password, 
-//             process.env.PASSWORD_SECRET
-//             ).toString();
-//     }
+router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
 
-//     try{
-//         const updatedUser = await User.findByIdAndUpdate(
-//             req.params.id, 
-//             {
-//                 $set: req.body
-//             },
-//             {
-//                 new: true
-//             },
-//             );
+    try{
+        const updatedProduct = await User.findByIdAndUpdate(
+            req.params.id, 
+            {
+                $set: req.body
+            },
+            {
+                new: true
+            },
+            );
 
-//             res.status(200).json(updatedUser);
-//     }catch(err){
-//         res.status(500).json(err)
-//     }
-// });
+            res.status(200).json(updatedUser);
+    }catch(err){
+        res.status(500).json(err)
+    }
+});
 
 // //DELETE
 // router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
