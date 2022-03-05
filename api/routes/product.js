6bb0917bacb53b7a334504c8e20757714f62b56a
PJMantoss <joelptoss@gmail.com>
2022-03-05 15:27:56 +0100

    update product route

--- api/routes/product.js
+++ api/routes/product.js
@@ -54,7 +54,7 @@ router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
 router.get("/find/:id", async (req, res) => {
     try {
       const product = await Product.findById(req.params.id);
-      const { password, ...others } = product._doc;
+      
       res.status(200).json(others);
     } catch (err) {
       res.status(500).json(err);
