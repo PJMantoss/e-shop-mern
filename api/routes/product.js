85869b6a909ed0b8c4e33acc3c5663124c345997
PJMantoss <joelptoss@gmail.com>
2022-03-05 15:24:40 +0100

    update product route

--- api/routes/product.js
+++ api/routes/product.js
@@ -41,10 +41,10 @@ router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
 });
 
 // //DELETE
-router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
     try{
-        await User.findByIdAndDelete(req.params.id)
-        res.status(200).json("User has been deleted...")
+        await Product.findByIdAndDelete(req.params.id)
+        res.status(200).json("Product has been deleted...")
     }catch(err){
         res.status(500).json(err);
     }
