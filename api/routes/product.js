09a035553e8fa540d6f10cf8c2f251957252dbc1
PJMantoss <joelptoss@gmail.com>
2022-03-05 15:28:47 +0100

    update product route

--- api/routes/product.js
+++ api/routes/product.js
@@ -61,6 +61,20 @@ router.get("/find/:id", async (req, res) => {
     }
   });
 
+//GET ALL PRODUCTS
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+    const query = req.query.new
+    try{
+        const users = query 
+            ? await User.find().sort({ _id: -1 }).limit(1) 
+            : await User.find();
+
+        res.status(200).json(users)
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+
 // //GET USERS STATS
 // router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
 //     const date = new Date();
