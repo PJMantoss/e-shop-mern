34e1ab9ccbe24b08a898f880f8df701a6942be6b
PJMantoss <joelptoss@gmail.com>
2022-03-06 21:38:54 +0100

    update product route

--- api/routes/cart.js
+++ api/routes/cart.js
@@ -51,7 +51,7 @@ router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
 });
 
 //GET USER CART
-router.get("/find/:id", async (req, res) => {
+router.get("/find/:id", verifyTokenAndAuthorization, async (req, res) => {
     try {
       const product = await Product.findById(req.params.id);
       
