d09d1912432103acdd88a5b4d36b7a224f5e1002
PJMantoss <joelptoss@gmail.com>
2022-03-07 13:32:22 +0100

    update order route

--- api/routes/order.js
+++ api/routes/order.js
@@ -75,7 +75,9 @@ router.get("/", verifyTokenAndAdmin, async (req, res) => {
 
 //GET MONTHLY INCOME
 router.get("/income", verifyTokenAndAdmin, async (req, res) => {
-    
+    const date = new Date();
+    const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
+    const prevMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1));
 });
 
 module.exports = router;
