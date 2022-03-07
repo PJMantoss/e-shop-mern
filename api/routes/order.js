968bdba3482a6243c1a8693ff6dfc9375a42e31c
PJMantoss <joelptoss@gmail.com>
2022-03-07 13:16:27 +0100

    update order route

--- api/routes/order.js
+++ api/routes/order.js
@@ -40,15 +40,15 @@ router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
     }
 });
 
-// // DELETE
-// router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
-//     try{
-//         await Cart.findByIdAndDelete(req.params.id)
-//         res.status(200).json("Cart has been deleted...")
-//     }catch(err){
-//         res.status(500).json(err);
-//     }
-// });
+// DELETE
+router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+    try{
+        await Cart.findByIdAndDelete(req.params.id)
+        res.status(200).json("Cart has been deleted...")
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
 
 // //GET USER CART
 // router.get("/find/:id", verifyTokenAndAuthorization, async (req, res) => {
