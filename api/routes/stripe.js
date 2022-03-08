3413591d4fca82773be88ad5141d4b23ed317e3c
PJMantoss <joelptoss@gmail.com>
2022-03-08 23:37:58 +0100

    update Stripe route

--- api/routes/stripe.js
+++ api/routes/stripe.js
@@ -9,9 +9,9 @@ router.post("/payment", (req, res) => {
         currency: "usd"
     }, (stripeErr, stripeRes) => {
         if(stripeErr){
-            res.status(500).json(stripeErr)
+            res.status(500).json(stripeErr);
         }else{
-            
+            res.status(200).json(stripeRes);
         }
     });
 });
