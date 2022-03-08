4037bfb60b3e6b0e1783c1bb6c0d08ca2721741f
PJMantoss <joelptoss@gmail.com>
2022-03-08 23:33:10 +0100

    update Stripe route

--- api/routes/stripe.js
+++ api/routes/stripe.js
@@ -3,7 +3,10 @@ const router = require("express").Router();
 const stripe = require("stripe")(process.env.STRIPE_KEY);
 
 router.post("/payment", (req, res) => {
-    stripe.create.charges({});
+    stripe.create.charges({
+        source: req.body.tokenId,
+        amount: req.body.amount
+    });
 });
 
 module.exports = router;
