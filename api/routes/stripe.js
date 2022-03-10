632ae66868cbf332197d7784adab1a617bcb47b0
PJMantoss <joelptoss@gmail.com>
2022-03-10 08:54:25 +0100

    update Stripe route

--- api/routes/stripe.js
+++ api/routes/stripe.js
@@ -6,7 +6,7 @@ router.post("/payment", (req, res) => {
     stripe.create.charges({
         source: req.body.tokenId,
         amount: req.body.amount,
-        currency: "usd"
+        currency: "usd",
     }, (stripeErr, stripeRes) => {
         if(stripeErr){
             res.status(500).json(stripeErr);
