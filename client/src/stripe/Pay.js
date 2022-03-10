8bb15bd193d9da31e9dec7ee4db71bb8fd2dadd2
PJMantoss <joelptoss@gmail.com>
2022-03-10 09:18:46 +0100

    update Pay

--- client/src/stripe/Pay.js
+++ client/src/stripe/Pay.js
@@ -10,20 +10,22 @@ const Pay = () => {
                 justifyContent: "center"
             }}
         >
-            <button
-                style={{
-                    border: "none",
-                    width: 120,
-                    borderRadius: 5,
-                    padding: "20px",
-                    backgroundColor: "black",
-                    color: "white",
-                    fontWeight: "600",
-                    cursor: "pointer"
-                }}
-            >
-                Pay Now
-            </button>
+            <StripeCheckout>
+                <button
+                    style={{
+                        border: "none",
+                        width: 120,
+                        borderRadius: 5,
+                        padding: "20px",
+                        backgroundColor: "black",
+                        color: "white",
+                        fontWeight: "600",
+                        cursor: "pointer"
+                    }}
+                >
+                    Pay Now
+                </button>
+            </StripeCheckout>
         </div>
     );
 }
