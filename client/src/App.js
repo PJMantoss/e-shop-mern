46d287dc6525f79627d65b18eef3feadc931e945
PJMantoss <joelptoss@gmail.com>
2022-03-13 17:08:25 +0100

    update App

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import Register from './pages/Register';
 import Login from './pages/Login';
 import Cart from './pages/Cart';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Pay from './stripe/Pay';
+import Success from './stripe/Success';
 
 
 const App = () => {
