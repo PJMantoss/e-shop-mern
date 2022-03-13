b569d9a117531c383dc60add9d910d6f419ab321
PJMantoss <joelptoss@gmail.com>
2022-03-13 17:08:03 +0100

    update Index

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Pay from './stripe/Pay';
-import Success from './stripe/Success';
 
 ReactDOM.render(
   <React.StrictMode>
