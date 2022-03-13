0ae03fbbb5862a0ca35e13eb8590fc51deb686f5
PJMantoss <joelptoss@gmail.com>
2022-03-13 17:35:15 +0100

    update server

--- api/server.js
+++ api/server.js
@@ -16,13 +16,13 @@ mongoose
     .then(() => console.log("DB Connection Successfull!"))
     .catch(err => console.log(err));
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
-app.use(cors());
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("Backend Server is running!");
