2f2d2219940b08b889254445100d86183094b5f7
PJMantoss <joelptoss@gmail.com>
2022-03-13 17:33:59 +0100

    update server

--- api/server.js
+++ api/server.js
@@ -7,6 +7,7 @@ const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
+const cors = require("cors");
 
 dotenv.config();
 
