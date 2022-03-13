a9f76b167ce2bcebf9bab45f38a9997100b74e24
PJMantoss <joelptoss@gmail.com>
2022-03-13 17:06:52 +0100

    update codes

---
+++ client/src/codes.js
@@ -0,0 +1,8 @@
+<Router>
+      <Routes>
+        <Route path="/pay">
+            <Pay />
+        </Route>
+        <Route path="/success" component={<Success />} />
+      </Routes>
+    </Router>
