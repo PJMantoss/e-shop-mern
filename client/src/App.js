dd37f6768fff87c3ab8ddc5d2591c9305ed55448
PJMantoss <joelptoss@gmail.com>
2022-03-16 09:14:16 +0100

    update App

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ const App = () => {
     <Router>
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route path="/" element={<ProductList />} />
       </Routes>
     </Router>
   );
