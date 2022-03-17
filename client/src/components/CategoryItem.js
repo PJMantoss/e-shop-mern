15fc577700bb52b4b5f8da2a424caf37495d2fcb
PJMantoss <joelptoss@gmail.com>
2022-03-17 13:18:23 +0100

    update CategoryItem

--- client/src/components/CategoryItem.js
+++ client/src/components/CategoryItem.js
@@ -47,7 +47,7 @@ const Button = styled.button`
 const CategoryItem = ({ item }) => {
   return (
     <Container>
-        <Link to={``}>
+        <Link to={`/products/${item.cat}`}>
             <Image src={item.img} />
             <Info>
                 <Title> {item.title} </Title>
