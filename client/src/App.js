8b81adadb16cdb9bc15950c0553910cde27dce60
PJMantoss <joelptoss@gmail.com>
2022-03-17 11:03:33 +0100

    update App

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Product from './pages/Product';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Cart from './pages/Cart';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Redirect } from 'react-router-dom';
 import Pay from './stripe/Pay';
 import Success from './stripe/Success';
 
