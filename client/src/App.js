d10c2a6ab3844a1840770d73ceb106a8a170a123
PJMantoss <joelptoss@gmail.com>
2022-03-17 11:04:36 +0100

    update App

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ const App = () => {
         <Route path="/products/:category" element={<ProductList />} />
         <Route path="/product/:id" element={<Product />} />
         <Route path="/cart" element={<Cart />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login"></Route>
         <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
