13eee254f64da0d64cb2ea22e8e9680d92b84d85
PJMantoss <joelptoss@gmail.com>
2022-03-17 13:20:27 +0100

    update App

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ const App = () => {
     <Router>
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route path="/products/:category" element={<ProductList />} />
+        <Route path="/products" element={<ProductList />} />
         <Route path="/product/:id" element={<Product />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/login" element = {user ? <Navigate to="/" /> : <Login />}/>
