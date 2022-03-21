1fd8914900fb22060bb0415ca822188d9f72cc7e
PJMantoss <joelptoss@gmail.com>
2022-03-21 12:46:27 +0100

    update Products

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -11,7 +11,7 @@ const Container = styled.div`
     padding: 20px;
 `;
 
-const Products = ({cat,filters,sort}) => {
+const Products = ({cat, filters, sort}) => {
   return (
     <Container>
         {popularProducts.map(item => (
