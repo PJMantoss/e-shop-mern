47a3126f05bfd610391cc24425667cd48a1fb416
PJMantoss <joelptoss@gmail.com>
2022-03-21 12:52:58 +0100

    update Products

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -16,6 +16,8 @@ const Products = ({cat, filters, sort}) => {
   const [products, setProducts] = useState([]);
   const [filteredProducts, setFilteredProducts] = useState([]);
 
+  useEffect(() => {}, [cat])
+
   return (
     <Container>
         {popularProducts.map(item => (
