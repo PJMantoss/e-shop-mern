6a31099bdb82756f6d6a761c9c3299ea8cf53462
PJMantoss <joelptoss@gmail.com>
2022-03-21 12:42:18 +0100

    update ProductList

--- client/src/pages/ProductList.js
+++ client/src/pages/ProductList.js
@@ -91,7 +91,7 @@ const ProductList = () => {
                 <Select onChange={e => setSort(e.target.value)}>
                     <Option value="newest">Newest</Option>
                     <Option value="asc">Price(asc)</Option>
-                    <Option value="">Price(desc)</Option>
+                    <Option value="desc">Price(desc)</Option>
                 </Select>
             </Filter>
         </FilterContainer>
