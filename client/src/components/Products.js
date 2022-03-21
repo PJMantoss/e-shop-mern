d0aab2d93f611503d73b3d9bb2e7a16d724643f8
PJMantoss <joelptoss@gmail.com>
2022-03-21 12:50:17 +0100

    update Products

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -12,7 +12,9 @@ const Container = styled.div`
 `;
 
 const Products = ({cat, filters, sort}) => {
+  
   const [products, setProducts] = useState();
+
   return (
     <Container>
         {popularProducts.map(item => (
