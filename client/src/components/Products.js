3df53e446508d3cd3a43f6b450696f0668d74616
PJMantoss <joelptoss@gmail.com>
2022-03-21 13:04:53 +0100

    update Products

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -20,7 +20,7 @@ const Products = ({cat, filters, sort}) => {
   useEffect(() => {
     const getProducts = async () => {
       try{
-        const response = await axios.get("http://localhost:5000/api/products");
+        const response = await axios.get(cat ? `http://localhost:5000/api/products?category=${cat}` : "http://localhost:5000/api/products");
         console.log(response);
       }catch(err){}
     }
