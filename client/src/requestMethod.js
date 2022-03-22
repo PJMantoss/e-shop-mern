6996e576faef3236fac595d109a73f3b02f2e418
PJMantoss <joelptoss@gmail.com>
2022-03-22 16:53:21 +0100

    update requestMethod file

--- client/src/requestMethod.js
+++ client/src/requestMethod.js
@@ -5,3 +5,8 @@ const BASE_URL = 'https://localhost:5000/api';
 export const publicRequest = axios.create({
     baseUrl: BASE_URL
 });
+
+export const userRequest = axios.create({
+    baseUrl: BASE_URL,
+    header: {token: 'Bearer '}
+});
