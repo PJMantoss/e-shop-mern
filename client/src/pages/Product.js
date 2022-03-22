e4a09eb5f8a688d363f640c3a6d254fd65e45b05
PJMantoss <joelptoss@gmail.com>
2022-03-22 16:44:41 +0100

    update Product page

--- client/src/pages/Product.js
+++ client/src/pages/Product.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import Announcement from '../components/Announcement';
 import Navbar from '../components/Navbar';
