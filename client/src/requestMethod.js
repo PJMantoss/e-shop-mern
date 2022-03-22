4664e8dcd6bb5012dfdb5e2a55bb046f8c179f79
PJMantoss <joelptoss@gmail.com>
2022-03-22 16:59:37 +0100

    update requestMethod file

--- client/src/requestMethod.js
+++ client/src/requestMethod.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const BASE_URL = 'https://localhost:5000/api';
-const TOKEN = 'https://localhost:5000/api';
+const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMWM5YWVkMDhjNDEzODY2NTYzNWViYiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0Nzk2NDY2OCwiZXhwIjoxNjQ4MjIzODY4fQ.fzt3YJ8sx7uyp7c5FRMSvgq0hU1rN6spPgwmXxcyrKM';
 
 export const publicRequest = axios.create({
     baseUrl: BASE_URL
@@ -9,5 +9,5 @@ export const publicRequest = axios.create({
 
 export const userRequest = axios.create({
     baseUrl: BASE_URL,
-    header: {token: 'Bearer '}
+    header: {token: `Bearer ${TOKEN}`}
 });
