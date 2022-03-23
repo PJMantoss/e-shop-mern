c1d6651825d47169e7575f93627719a0f1edd857
PJMantoss <joelptoss@gmail.com>
2022-03-23 16:04:06 +0100

    add cartRedux component

--- client/src/redux/cartRedux.js
+++ client/src/redux/cartRedux.js
@@ -2,5 +2,9 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const cartSlice = createSlice({
     name: "cart",
-    intialState: {}
+    intialState: {
+        products: [],
+        quantity: 0,
+        total: 0
+    },
 });
