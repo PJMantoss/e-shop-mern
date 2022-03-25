ae30bb84c482185242e6b3ee6c3a9a9dfd7d409c
PJMantoss <joelptoss@gmail.com>
2022-03-25 18:04:09 +0100

    update Product page

--- client/src/pages/Product.js
+++ client/src/pages/Product.js
@@ -157,7 +157,7 @@ const Product = () => {
     };
 
     const handleClick = () => {
-        dispatch(addProduct({ product, quantity, price:product.price }));
+        dispatch(addProduct({ product, quantity, price:product.price * price}));
     }
 
   return (
