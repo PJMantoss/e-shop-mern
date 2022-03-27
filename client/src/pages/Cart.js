c74e6de0b6e661b16060ca6bea0d4a596155a876
PJMantoss <joelptoss@gmail.com>
2022-03-27 16:56:20 +0100

    update Cart

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Announcement from '../components/Announcement';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
