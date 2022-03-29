69e236b3291602d9ce93ae51d30ce9b81f09f8e4
PJMantoss <joelptoss@gmail.com>
2022-03-29 12:53:55 +0100

    update Cart

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -185,7 +185,7 @@ const Cart = () => {
             }
         };
 
-        stripeToken && cart.total >= 1 && makeRequest();
+        stripeToken && makeRequest();
     }, [stripeToken, cart.total, navigate])
 
   return (
