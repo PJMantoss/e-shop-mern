29a5a98ed27ffd86b4dd0e115718cbada474ce29
PJMantoss <joelptoss@gmail.com>
2022-03-30 12:25:40 +0100

    update userSlice

--- client/src/redux/userSlice.js
+++ client/src/redux/userSlice.js
@@ -8,11 +8,7 @@ const userSlice = createSlice({
         error: false
     },
     reducers: {
-        loginStart: (state) => {
-            state.quantity += 1;
-            state.product.push(action.payload.product);
-            state.total += action.payload.price * action.payload.quantity
-        },
+        loginStart: (state) => {},
     }
 });
 
