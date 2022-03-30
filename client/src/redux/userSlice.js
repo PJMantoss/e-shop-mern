7ec445f55b3fc3e4b9bfd44879147c5fbba7ae55
PJMantoss <joelptoss@gmail.com>
2022-03-30 12:32:17 +0100

    update userSlice

--- client/src/redux/userSlice.js
+++ client/src/redux/userSlice.js
@@ -22,5 +22,5 @@ const userSlice = createSlice({
     }
 });
 
-export const { loginStart, loginSuccess } = userSlice.actions;
+export const { loginStart, loginSuccess, loginFailure } = userSlice.actions;
 export default userSlice.reducer;
