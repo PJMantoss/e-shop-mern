22ad437ab9eef91e762047837b353a390c340420
PJMantoss <joelptoss@gmail.com>
2022-03-30 12:29:40 +0100

    update userSlice

--- client/src/redux/userSlice.js
+++ client/src/redux/userSlice.js
@@ -12,7 +12,8 @@ const userSlice = createSlice({
             state.isFetching = true
         },
         loginSuccess: (state, action) => {
-            state.isFetching = false
+            state.isFetching = false,
+            state.currentUser = action.payload
         },
         loginFailure: (state) => {},
     }
