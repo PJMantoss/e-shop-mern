9bfdc5a458c161a12a62c68b49c4f16a588bee39
PJMantoss <joelptoss@gmail.com>
2022-03-31 18:06:07 +0100

    update apiCalls.js

--- client/src/redux/apiCalls.js
+++ client/src/redux/apiCalls.js
@@ -1,7 +1,7 @@
 import { loginStart, loginSuccess, loginFailure  } from "./userRedux";
 import { publicRequest } from "../requestMethods";
 
-export const login = async (dispatch, user){
+export const login = async (dispatch, user) => {
     dispatch(loginStart());
     try{
         const response = await publicRequest.post("/auth/login", user);
