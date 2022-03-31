2e48dd80821f64bf1d72dd5a82f573ef5288cc40
PJMantoss <joelptoss@gmail.com>
2022-03-31 18:01:15 +0100

    update apiCalls.js

--- client/src/redux/apiCalls.js
+++ client/src/redux/apiCalls.js
@@ -5,6 +5,7 @@ export const login = async (dispatch, user){
     dispatch(loginStart());
     try{
         const response = await publicRequest.post("/auth/login", user);
+        dispatch();
     }catch(err){
         console.log(err);
     };
