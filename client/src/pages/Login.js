88951c4a6f53f007d9b42af904840400c58c3026
PJMantoss <joelptoss@gmail.com>
2022-04-01 15:39:22 +0100

    update Login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -63,7 +63,7 @@ const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const dispatch = useDispatch();
-  const { isFetching, error } = useSelector();
+  const { isFetching, error } = useSelector(state => state.user);
 
   const handleClick = (e) => {
     e.preventDefault();
