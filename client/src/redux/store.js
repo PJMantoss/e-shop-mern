e3135b0fe2e9135b03155aefdb91884fc5d1bc02
PJMantoss <joelptoss@gmail.com>
2022-04-02 18:34:13 +0100

    update store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -19,7 +19,7 @@ const persistConfig = {
     storage,
   }
   
-const persistedReducer = persistReducer(persistConfig, rootReducer)
+const persistedReducer = persistReducer(persistConfig, userReducer)
 
 export default configureStore({
     reducer: {
