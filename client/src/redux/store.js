f1091f4fd2de5c4ba3130d87e0af4ce5d218d74d
PJMantoss <joelptoss@gmail.com>
2022-04-02 18:52:19 +0100

    update store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -20,7 +20,7 @@ const persistConfig = {
     storage,
   };
 
-const rootReducer = combineReducers()
+const rootReducer = combineReducers({ user: userReducer, cart: cartReducer })
   
 const persistedReducer = persistReducer(persistConfig, userReducer);
 
