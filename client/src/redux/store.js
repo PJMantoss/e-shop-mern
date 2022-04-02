48d0c786f83c7a0ffdf1e6b38593e7b40ba73ac1
PJMantoss <joelptoss@gmail.com>
2022-04-02 18:43:10 +0100

    update store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -21,7 +21,7 @@ const persistConfig = {
   
 const persistedReducer = persistReducer(persistConfig, userReducer);
 
-export store = () => configureStore({
+export const store = () => configureStore({
     reducer: {
         cart: cartReducer,
         user: persistedReducer
