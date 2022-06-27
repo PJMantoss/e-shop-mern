801b481411250e7eb75e2bd58271eb519a9ecc09
PJMantoss <joelptoss@gmail.com>
2022-06-27 18:34:48 +0100

    update product route

--- api/routes/product.js
+++ api/routes/product.js
@@ -24,7 +24,7 @@ router.post("/", verifyTokenAndAdmin, async (req, res) => {
 router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
 
     try{
-        const updatedProduct = await User.findByIdAndUpdate(
+        const updatedProduct = await Product.findByIdAndUpdate(
             req.params.id, 
             {
                 $set: req.body
