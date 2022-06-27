a78dbafc83ebb8eb3befede91ef0177dfda7c7e9
PJMantoss <joelptoss@gmail.com>
2022-06-27 18:11:02 +0100

    update user route

--- api/routes/user.js
+++ api/routes/user.js
@@ -56,10 +56,11 @@ router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
 
 //GET ALL USERS
 router.get("/", verifyTokenAndAdmin, async (req, res) => {
-    const query = req.query.new
+    const query = req.query.new;
+
     try{
         const users = query 
-            ? await User.find().sort({ _id: -1 }).limit(1) 
+            ? await User.find().sort({ _id: -1 }).limit(5) 
             : await User.find();
 
         res.status(200).json(users)
