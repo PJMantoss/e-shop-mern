b4dced2181327bd3f8680851401738298fad0f79
PJMantoss <joelptoss@gmail.com>
2022-06-27 18:31:33 +0100

    update user route

--- api/routes/user.js
+++ api/routes/user.js
@@ -10,7 +10,7 @@ const router = require("express").Router();
 //UPDATE
 router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
     if(req.body.password){
-        req.body.password= CryptoJS.AES.encrypt(
+        req.body.password = CryptoJS.AES.encrypt(
             req.body.password, 
             process.env.PASSWORD_SECRET
             ).toString();
