efdfb075708f1210fe473ed28e77b685a31420d5
PJMantoss <joelptoss@gmail.com>
2022-06-28 19:37:22 +0100

    update order route

--- api/routes/order.js
+++ api/routes/order.js
@@ -77,11 +77,11 @@ router.get("/", verifyTokenAndAdmin, async (req, res) => {
 router.get("/income", verifyTokenAndAdmin, async (req, res) => {
     const date = new Date();
     const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
-    const prevMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1));
+    const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1));
 
     try{
         const income = await Order.aggregate([
-            { $match: { createdAt: { $gte: prevMonth } } },
+            { $match: { createdAt: { $gte: previousMonth } } },
             {
                 $project: {
                     month: {$month: "$createdAt"},
