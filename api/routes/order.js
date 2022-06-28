d7dea1e279a749f16f956455d10c4900dfc57d40
PJMantoss <joelptoss@gmail.com>
2022-06-28 19:32:01 +0100

    update order route

--- api/routes/order.js
+++ api/routes/order.js
@@ -73,7 +73,7 @@ router.get("/", verifyTokenAndAdmin, async (req, res) => {
     }
 });
 
-//GET MONTHLY INCOME
+//STATS - GET MONTHLY INCOME
 router.get("/income", verifyTokenAndAdmin, async (req, res) => {
     const date = new Date();
     const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
