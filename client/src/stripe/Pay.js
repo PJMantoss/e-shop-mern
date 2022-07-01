991fa7224368e56572dcd5a8dfa191495447edf3
PJMantoss <joelptoss@gmail.com>
2022-07-01 18:38:36 +0100

    update Pay

--- client/src/stripe/Pay.js
+++ client/src/stripe/Pay.js
@@ -71,7 +71,7 @@ const Pay = () => {
                             Pay Now
                         </button>
                     </StripeCheckout>
-                // )}
+                
         </div>
     );
 }
