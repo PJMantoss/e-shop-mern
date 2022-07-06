1e3094b5435bd664524bed9781d710374a1aabf0
PJMantoss <joelptoss@gmail.com>
2022-07-06 17:14:41 +0100

    update Product page

--- client/src/pages/Product.js
+++ client/src/pages/Product.js
@@ -139,7 +139,7 @@ const Product = () => {
     useEffect(() => {
         const getProduct = async () => {
             try{
-                const response = await publicRequest.get("/products/find/" + id);
+                const response = await publicRequest.get("/products/find/"+id);
                 setProduct(response.data);
             }catch{}
         }
