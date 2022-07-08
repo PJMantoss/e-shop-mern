d5930ef6e308dfb0313c6f9cb9b050fe6eec29e7
PJMantoss <joelptoss@gmail.com>
2022-07-08 13:35:19 +0100

    update TOKEN

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const BASE_URL = 'http://localhost:5000/api/';
-const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyYjc0ZjhmZmFkNWZiYjY0NTIxNmNkZCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1NzExNjAxMiwiZXhwIjoxNjU3Mzc1MjEyfQ.40jBtdin0eq21td8KkwRf0aY_Ku0YGy8ITVrB8g5HQI';
+const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyYjc0ZjhmZmFkNWZiYjY0NTIxNmNkZCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1NzI4MzUzNSwiZXhwIjoxNjU3NTQyNzM1fQ.IWGVpv2H5COoGHzPgi7SNuWCiOXaeUxd8GHXH7_5goU';
 
 export const publicRequest = axios.create({
     baseUrl: BASE_URL,
