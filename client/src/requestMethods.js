fdbd057740ef77657b8bb0d4bef8cab68da0c7bf
PJMantoss <joelptoss@gmail.com>
2022-07-09 19:39:54 +0100

    update cart page

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const BASE_URL = 'http://localhost:5000/api/';
-const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyYjc0ZjhmZmFkNWZiYjY0NTIxNmNkZCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1NzI4MzUzNSwiZXhwIjoxNjU3NTQyNzM1fQ.IWGVpv2H5COoGHzPgi7SNuWCiOXaeUxd8GHXH7_5goU';
+const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyYjc0ZjhmZmFkNWZiYjY0NTIxNmNkZCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1NzM5MTk0NiwiZXhwIjoxNjU3NjUxMTQ2fQ.2pI9FR6pDJdzPCvRyYPP8hl0yOtk9PECf9wy05K-Hq0';
 
 export const publicRequest = axios.create({
     baseUrl: BASE_URL,
